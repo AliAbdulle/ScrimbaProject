a186b8941072fb765ac882cf035067db0173dc6c
AliAbdulle <alidhobey@gmail.com>
2020-03-21 20:25:08 -0500

    Remove my info from index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyInfo from './MyInfo'
 //import './App.css';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import MyInfo from "./components/MyInfo";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<App />,
-<MyInfo />, document.getElementById('root'));
+<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
