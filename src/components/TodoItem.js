d69b5c24a7291816de23e0481578b05e1afe5f2f
AliAbdulle <alidhobey@gmail.com>
2020-03-23 20:19:44 -0500

    style css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function TodoItem() {
     return (
-      <div className="Todo-list"> 
+      <div className="Todo-item"> 
       <input type="checkbox" />
       <p>Placeholder text here</p>
 
