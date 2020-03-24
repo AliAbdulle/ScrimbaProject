102722d340c523d96b429620bf86c19232335e11
AliAbdulle <alidhobey@gmail.com>
2020-03-23 21:15:44 -0500

    Picture come up too long need to fix

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,41 @@ import Footer from './Footer';
 //import './App.css';
 //App function
 function App() {
-  
+
   return (
     <div className="App">
       <Nav className="nav-bar"/>
-      <TodoItem className="Todo-list"/>
+      </div>
+      // {/* <TodoItem className="Todo-list"/> */}
+      <div className="contacts">
+            <ContactCard
+                name="Mr. Whiskerson"
+                imgUrl="http://placekitten.com/300/200"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+
+            <ContactCard
+                name="Fluffykins"
+                imgUrl="http://placekitten.com/400/200"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+
+            <ContactCard
+                name="Destroyer"
+                imgUrl="http://placekitten.com/400/300"
+                phone="(212) 555-3456"
+                email="ofworlds@yahoo.com"
+            />
+            <ContactCard
+                name="Felix"
+                imgUrl="http://placekitten.com/200/100"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+
+        </div>  
       <Footer />
     </div>
   );
