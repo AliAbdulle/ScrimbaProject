0b7ebf68294cbfc35f6e1bd4cc17762bbefc3215
AliAbdulle <alidhobey@gmail.com>
2020-03-24 19:58:33 -0500

    joke component file

---
+++ src/components/Joke.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Joke(){
+    return (
+        <div> 
+            <h3 style={{display: !props.question }}></h3>
+        </div>
+    )
+}
+
+export default Joke;
