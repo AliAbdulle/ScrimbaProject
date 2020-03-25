87d1d4aba488027d819652659a0acf62aa3c7bb7
AliAbdulle <alidhobey@gmail.com>
2020-03-24 20:05:05 -0500

    Add new component and run it

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import TodoItem from './TodoItem'
+//import TodoItem from './TodoItem'
 import Nav from './Nav'
+import Joke from './Joke.js';
 import Footer from './Footer';
 import ContactCard from './ContactCard';
 //import './App.css';
@@ -41,6 +42,35 @@ function App() {
                 email="thecat@hotmail.com"
             />
 
+        </div>
+        <div>
+            <Joke punchLine="It’s hard to explain puns to kleptomaniacs because they always take things literally." />
+            
+            <Joke 
+                question="What's the best thing about Switzerland?" 
+                punchLine="I don't know, but the flag is a big plus!"
+            />
+            
+            <Joke 
+                question="Did you hear about the mathematician who's afraid of negative numbers?" 
+                punchLine="He'll stop at nothing to avoid them!"
+            />
+            
+            <Joke 
+                question="Hear about the new restaurant called Karma?" 
+                punchLine="There’s no menu: You get what you deserve."
+            />
+            
+            <Joke 
+                question="Did you hear about the actor who fell through the floorboards?" 
+                punchLine="He was just going through a stage."
+            />
+            
+            <Joke 
+                question="Did you hear about the claustrophobic astronaut?" 
+                punchLine="He just needed a little space."
+            />
+            
         </div>
       <Footer />
       </div>
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Joke(){
+function Joke(props){
     return (
         <div> 
-            <h3 style={{display: !props.question }}></h3>
+            <h3 style={{display: !props.question && "none"}}> question: {props.question}</h3>
+            <h3>Answer: {props.punchLine}</h3>
+            <hr/>
         </div>
     )
 }
