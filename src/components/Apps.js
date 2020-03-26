c795eaeea331258723795e98ee9852a79ca8ede8
AliAbdulle <alidhobey@gmail.com>
2020-03-26 16:37:33 -0500

    Todos added event handle change

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,38 +1,86 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+import todosData from "./todosData"
 
 class Apps extends Component {
-    constructor(){
+
+
+    constructor() {
         super()
         this.state = {
-            count: 0
+            todos: todosData
         }
-        this.handleClick = this.handleClick.bind(this)
-        this.handleClicked = this.handleClicked.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
     
-    handleClick() {
+    handleChange(id) {
         this.setState(prevState => {
-            return {
-                count: prevState.count + 1
-            }
+            const updateState = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo
+            })
+            return{
+                todos: updateState
+            } 
         })
+        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
     }
-    handleClicked() {
-        this.setState(prevState => {
-            return {
-                count: prevState.count - 1
-            }
-        })
-    }
+    
     render() {
-        return(
-            <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.handleClick}>Change!</button>
-                <button onClick={this.handleClicked}>Nagative!</button>
+        const todoItems= this.state.todos.map(item => <TodoItem key={item.id} item={item}
+        handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
             </div>
-        )
+        )    
     }
+
+
+
+
+
+
+
+
+
+
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         count: 0
+    //     }
+    //     this.handleClick = this.handleClick.bind(this)
+    //     this.handleClicked = this.handleClicked.bind(this)
+    // }
+    
+    // handleClick() {
+    //     this.setState(prevState => {
+    //         return {
+    //             count: prevState.count + 1
+    //         }
+    //     })
+    // }
+    // handleClicked() {
+    //     this.setState(prevState => {
+    //         return {
+    //             count: prevState.count - 1
+    //         }
+    //     })
+    // }
+    // render() {
+    //     return(
+    //         <div>
+    //             <h1>{this.state.count}</h1>
+    //             <button onClick={this.handleClick}>Change!</button>
+    //             <button onClick={this.handleClicked}>Nagative!</button>
+    //         </div>
+    //     )
+    // }
 
 }
 export default Apps
