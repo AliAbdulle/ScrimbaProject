424b02ccdcc8c4feb13f56cc4d54a7cd90387dc6
AliAbdulle <alidhobey@gmail.com>
2020-03-26 17:44:13 -0500

    changed color when color changed

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,73 +1,85 @@
-import React, { Component } from 'react'
-import TodoItem from './TodoItem';
-import todosData from "./todosData"
+import React, { Component } from 'react';
+// import TodoItem from './TodoItem';
+// import todosData from "./todosData"
+import randomcolor from "randomcolor";
 
 class Apps extends Component {
 
 
-    constructor() {
-        super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-    
-    handleChange(id) {
-        this.setState(prevState => {
-            const updateState = prevState.todos.map(todo => {
-                if (todo.id === id) {
-                    return {
-                            ...todo,
-                            completed: !todo.completed
-                    } 
-                }
-                return todo
-            })
-            return{
-                todos: updateState
-            } 
-        })
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-    }
-    
-    render() {
-        const todoItems= this.state.todos.map(item => <TodoItem key={item.id} item={item}
-        handleChange={this.handleChange}/>)
-        
-        return (
-            <div className="todo-list">
-                {todoItems}
-            </div>
-        )    
-    }
-
-
-
-
-
-
-
-
-
-
-    // constructor(){
+    // constructor() {
     //     super()
     //     this.state = {
-    //         count: 0
+    //         todos: todosData
     //     }
-    //     this.handleClick = this.handleClick.bind(this)
-    //     this.handleClicked = this.handleClicked.bind(this)
+    //     this.handleChange = this.handleChange.bind(this)
     // }
     
-    // handleClick() {
+    // handleChange(id) {
     //     this.setState(prevState => {
-    //         return {
-    //             count: prevState.count + 1
-    //         }
+    //         const updateState = prevState.todos.map(todo => {
+    //             if (todo.id === id) {
+    //                 return {
+    //                         ...todo,
+    //                         completed: !todo.completed
+    //                 } 
+    //             }
+    //             return todo
+    //         })
+    //         return{
+    //             todos: updateState
+    //         } 
     //     })
+    //     // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+    //     // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
     // }
+    
+    // render() {
+    //     const todoItems= this.state.todos.map(item => <TodoItem key={item.id} item={item}
+    //     handleChange={this.handleChange}/>)
+        
+    //     return (
+    //         <div className="todo-list">
+    //             {todoItems}
+    //         </div>
+    //     )    
+    // }
+
+
+
+
+
+
+
+
+
+
+    constructor(){
+        super()
+        this.state = {
+            count: 0,
+            color: ""
+        }
+        this.handleClick = this.handleClick.bind(this)
+        // this.handleClicked = this.handleClicked.bind(this)
+    }
+    
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !==this.state.count) {
+            const newColor = randomcolor()
+            this.setState({color: newColor})
+
+        }
+    }
+
+
     // handleClicked() {
     //     this.setState(prevState => {
     //         return {
@@ -75,15 +87,15 @@ class Apps extends Component {
     //         }
     //     })
     // }
-    // render() {
-    //     return(
-    //         <div>
-    //             <h1>{this.state.count}</h1>
-    //             <button onClick={this.handleClick}>Change!</button>
-    //             <button onClick={this.handleClicked}>Nagative!</button>
-    //         </div>
-    //     )
-    // }
+    render() {
+        return(
+            <div>
+                <h1 style={{color:this.state.color}}>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
+                <button onClick={this.handleClicked}>Nagative!</button>
+            </div>
+        )
+    }
 
 }
 export default Apps
