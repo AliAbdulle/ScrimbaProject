c6900cd28709ee1252e9c2eab7188951a63a2b12
AliAbdulle <alidhobey@gmail.com>
2020-03-26 15:49:58 -0500

    bind the state to check if num is changed

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -6,16 +6,21 @@ class Apps extends Component {
         this.state = {
             count: 0
         }
+        this.handleClick = this.handleClick.bind(this)
     }
     
     handleClick() {
-        this.setState
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
     render() {
         return(
             <div>
                 <h1>{this.state.count}</h1>
-                <button onClick={this}>Change!</button>
+                <button onClick={this.handleClick}>Change!</button>
             </div>
         )
     }
