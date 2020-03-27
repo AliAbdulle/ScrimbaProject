ae9bcb16a787d260d1e51eac849f0a03d3817c86
AliAbdulle <alidhobey@gmail.com>
2020-03-27 18:49:28 -0500

    cool stuff is happing

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 function Conditional(props) {
-    return (
-        <h1> Hello</h1>
-    )
-}
+    if(props.isLoading) {
+        return (
+            <h1>Is Loading.....</h1>
+        )
+    }
+    else {
+
+        return (
+            <h1> Some cool stuff about conditional rendering</h1>
+        )
+    }
+    }
 
 export default Conditional;
