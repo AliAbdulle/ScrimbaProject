90cb54175925007ed8ff69313ccad6ca4c0e76bc
AliAbdulle <alidhobey@gmail.com>
2020-03-27 18:58:07 -0500

    Conditional renderding using JSX

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 
 function Conditional(props) {
-    if(props.isLoading) {
-        return (
-            <h1>Is Loading.....</h1>
-        )
-    }
-    else {
+    return (
+        <div>
+            {props.isLoading ? <h1>Is Loading.....</h1> : <h1> Some cool stuff about conditional rendering</h1>}
+        </div>
+    )
+    // if(props.isLoading === true) {
+    //     return (
+    //         <h1>Is Loading.....</h1>
+    //     )
+    // }
+    // else {
 
-        return (
-            <h1> Some cool stuff about conditional rendering</h1>
-        )
-    }
+    //     return (
+    //         <h1> Some cool stuff about conditional rendering</h1>
+    //     )
+    // }
     }
 
 export default Conditional;
