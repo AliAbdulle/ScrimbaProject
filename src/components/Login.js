6aa03e53f21769976f890c908f3ab14f6cd4a9b0
AliAbdulle <alidhobey@gmail.com>
2020-03-27 20:37:36 -0500

    text display the page

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,11 +17,12 @@ class Login extends Component {
     }
 
     render(){
-        let textWritten = this.state.isLoggedIn ? "Logout" : "Login"
+        let textWritten = this.state.isLoggedIn ? "Logout" : "Login";
         let textDisplay = this.state.isLoggedIn ? "Logged in" : "Logged out"
         return (
             <div>
                 <button className="log" onClick={this.handleChanged}>{textWritten}</button>
+                 <h1>{textDisplay}</h1>
                 
             </div>
         )
