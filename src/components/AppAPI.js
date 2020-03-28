16299b642065bdb5b2c7b74dd3738a00905e676d
AliAbdulle <alidhobey@gmail.com>
2020-03-27 20:52:48 -0500

    data objects received from the API

--- src/components/AppAPI.js
+++ src/components/AppAPI.js
@@ -7,6 +7,12 @@ class AppAPI extends Component {
 
         }
     }
+    
+    componentDidMount() {
+        fetch("https://swapi.co/api/people/1")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
 
     render(){
         return (
