8d9a140818f90935773e37efc2a5bfcd4a601a8e
AliAbdulle <alidhobey@gmail.com>
2020-03-27 19:52:34 -0500

    Hard code text changed and css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,20 @@
 import React, { Component } from 'react';
 
+class Login extends Component {
+    constructor(){
+        super()
+        this.state = {
+            isLoggedIn : true
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <button className="log">Log In</button>
+            </div>
+        )
+    }
+}
+
 export default Login;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import Tapps from './components/Tapps';
 // import Apps from './components/Apps';
 import * as serviceWorker from './serviceWorker';
+import Login from './components/Login';
 
 ReactDOM.render(
-<Tapps />, document.getElementById('root'));
+<Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
