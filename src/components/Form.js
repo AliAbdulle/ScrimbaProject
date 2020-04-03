f38da2c20364df3fb0aed280d53b0f424644bc69
AliAbdulle <alidhobey@gmail.com>
2020-04-03 16:53:48 -0500

    event handle created and post to dom

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    state = { 
-        firstName : ""
+    constructor(){
+        super()
+    this.state = {
+        firstName : "",
+        lastName:""
+     }
+     this.handleChange = this.handleChange.bind(this)
+    }
+
+     handleChange(event) {
+         this.setState({
+             [event.target.name]: event.target.value
+         })
      }
     render() { 
         return ( 
             <form>
-                <input type="text" placeholder="First Name" />
+                <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange}/>
+                <input type="text" name="lastName"placeholder="Last Name" onChange={this.handleChange}/>
+                <br />
+                <h1>{this.state.firstName}</h1>
+                <h1>{this.state.lastName}</h1>
             </form>
          );
     }
