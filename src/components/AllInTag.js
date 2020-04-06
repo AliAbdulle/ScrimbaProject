3b77f35386d5508f7b6f61571ac03b66ff93db17
AliAbdulle <alidhobey@gmail.com>
2020-04-05 22:16:16 -0500

    Life order to check

--- src/components/AllInTag.js
+++ src/components/AllInTag.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react'
 
 
+class AllInTag extends Component {
+    render() {
+        return (
+            <p>Tag in all</p>
+        )
+    }
+}
+
+
 export default AllInTag;
