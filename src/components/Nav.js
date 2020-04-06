9cebfb297bf255eaf0eafcdd8758079eacbd61b7
AliAbdulle <alidhobey@gmail.com>
2020-04-05 21:58:34 -0500

    deleted the form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Nav() {
     return (
-        <div className="Navelink">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Navbar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,13 +31,8 @@ function Nav() {
               <a className="nav-link disabled" href="#">Disabled</a>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-      </div>
+      </nav>,
+     
             )
         }
         export default Nav;
