8da192b33dd1a28324dbbd31016710e32be19cd1
AliAbdulle <alidhobey@gmail.com>
2020-04-08 14:51:28 -0500

    Project meme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/Project/App';
 //import Tapps from './components/Tapps';
 // import Apps from './components/Apps';
-import * as serviceWorker from './serviceWorker';
 //import Login from './components/Login';
 //import Appapi from './components/Appapi';
 //import Form from './components/Form';
-import AllInTag from './components/Forms/AllInTag';
+//import AllInTag from './components/Forms/AllInTag';
+import * as serviceWorker from './serviceWorker';
 //App Render
 ReactDOM.render(
-<AllInTag />, document.getElementById('root'));
+<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
