3d20f2d74ad3f22427b36c7a0ad651e43387c285
AliAbdulle <alidhobey@gmail.com>
2020-04-08 17:00:49 -0500

    Increment number of the file

---
+++ src/components/Like/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+
+
+function App(){
+    const [count, setCount]
+    return(
+        <div>
+            <h1 style={{color: color}}>{count}</h1>
+            <button onClick={increment}> Increment</button>
+            <button onClick={decrement}> Decrement</button>
+        </div>
+    )
+
+}
+
+export default App
