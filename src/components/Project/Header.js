aa360d950691dba259634e908d2c40dbb81187b5
AliAbdulle <alidhobey@gmail.com>
2020-04-09 15:42:32 -0500

    Saved original one

--- src/components/Project/Header.js
+++ src/components/Project/Header.js
@@ -7,7 +7,7 @@ function Header() {
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                 alt="Problem?"
                 />
-                <h3>Meme Generator</h3>
+                <p>Meme Generator</p>
         </header>
     )
 
